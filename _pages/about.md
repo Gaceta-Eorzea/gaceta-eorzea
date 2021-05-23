a2c0c740e576dad63a8bff25eb842b976576b4fa
Juan Medina <jamedina@gmail.com>
2021-05-23 07:05:38 +0100

    update icon

--- _pages/about.md
+++ _pages/about.md
@@ -6,3 +6,5 @@ comments: true
 ---
 
 La Gaceta Eorzea es una web de Articulos y guias de Final Fantasy XIV en Español sin animo de lucro y con la idea de fomentar una comunidad hispana en el juego.
+
+ Nos puedes seguir en nuestra cuenta de <a href="https://twitter.com/GazetaEorzea"><i class="fab fa-twitter"></i>twitter</a> para estar al dia de las actualizaciones de nuestros articulos.
--- docs/assets/js/lunrsearchengine.js
+++ docs/assets/js/lunrsearchengine.js
@@ -8,7 +8,7 @@ var documents = [{
     "id": 1,
     "url": "/about",
     "title": "Acerca de la Gaceta Eorzea",
-    "body": "La Gaceta Eorzea es una web de Articulos y guias de Final Fantasy XIV en Español sin animo de lucro y con la idea de fomentar una comunidad hispana en el juego. "
+    "body": "La Gaceta Eorzea es una web de Articulos y guias de Final Fantasy XIV en Español sin animo de lucro y con la idea de fomentar una comunidad hispana en el juego. Nos puedes seguir en nuestra cuenta de twitter para estar al dia de las actualizaciones de nuestros articulos. "
     }, {
     "id": 2,
     "url": "/categories",
@@ -33,7 +33,7 @@ var documents = [{
     "id": 6,
     "url": "/fashion210521/",
     "title": "Fashion Report 21 Mayo 2021",
-    "body": "2021/05/22 - Una semana mas Kaiyoko Star ha publicado la solucion para el Fashion Report, la cual podeis veir a continuacion.  Para hacer los 80 puntos y ganar 60,000 MGP necesitareis :    En la pechera The Emperor’s New Robe la cual se la podeis comprar a Tataroga en Mor Dhona (X:22. 1 Y:6. 7), sino lo podeis comprar porque no teneis desbloqueada la opcion cualquier jugador os lo puede comprar y vale solo 71 gil.     En guantes el tinte Hunter Green Dye el cual se lo podeis comprar a Ixali Vendor en North Shroud (X:24. 9 Y:22. 7) si teneis la reputacion de los Ixali subida o sino comprarlo en el Market Board ya que no suele ser muy caro.     En pantalones el tinte Ceruleum Blue Dye el cual se lo podeis comprar a Unsynrael en Limsa Lominsa Lower Decks (X:6. 0 Y:12. 3), Alaric en Old Gridania (X:14. 3 Y:8. 8) o Nanabe en Ul’dah - Steps of Thal (X:14. 2 Y:11. 0).  Recordar que teneis ademas que tener equipado un item en cada slot del equipo, a excepcion de off-hand. Sino teneis desbloqueados el fashion report lo podeis hacer mediante la quest Passion for Fashion la cual os la da Lewena en The Gold Saucer (X: 4. 8 Y: 6. 1). Por ultimo recordar que este año tenemos de nuevo el evento del Final Fantasy XV con el que podeis conseguir el coche Regalia Type-G por 200,000 MGP.  "
+    "body": "2021/05/22 - Una semana mas Kaiyoko Star ha publicado la solucion para el Fashion Report, la cual podeis veir a continuacion.  Para hacer los 80 puntos y ganar 60,000 MGP necesitareis :    En la pechera The Emperor’s New Robe la cual se la podeis comprar a Tataroga en Mor Dhona (X:22. 1 Y:6. 7), sino lo podeis comprar porque no teneis desbloqueada la opcion cualquier jugador os lo puede comprar y vale solo 71 gil.     En guantes el tinte Hunter Green Dye el cual se lo podeis comprar a Ixali Vendor en North Shroud (X:24. 9 Y:22. 7) si teneis la reputacion de los Ixali subida o sino comprarlo en el Market Board ya que no suele ser muy caro.     En pantalones el tinte Ceruleum Blue Dye el cual se lo podeis comprar a Unsynrael en Limsa Lominsa Lower Decks (X:6. 0 Y:12. 3), Alaric en Old Gridania (X:14. 3 Y:8. 8) o Nanabe en Ul’dah - Steps of Thal (X:14. 2 Y:11. 0).  Recordar que teneis ademas que tener equipado un item en cada slot del equipo, a excepcion de off-hand. Sino teneis desbloqueados el fashion report lo podeis hacer mediante la quest Passion for Fashion la cual os la da Lewena en The Gold Saucer (X: 4. 8 Y: 6. 1). Por ultimo recordar que este año tenemos de nuevo el evento del Final Fantasy XV con el que podeis conseguir el coche Regalia Type-G, el cual es para 4 jugadores, y os costara 200,000 MGP por lo cual hacer el fashion report es una manera facil de conseguirlo.  "
     }, {
     "id": 7,
     "url": "/traducir/",
