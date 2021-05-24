4cebf8a7936813d7deb7f7958645407e359fc474
Juan Medina <jamedina@gmail.com>
2021-05-24 18:57:48 +0100

    actualizacion de acerca de

--- docs/assets/js/lunrsearchengine.js
+++ docs/assets/js/lunrsearchengine.js
@@ -8,7 +8,7 @@ var documents = [{
     "id": 1,
     "url": "/about",
     "title": "Acerca de la Gaceta Eorzea",
-    "body": "La Gaceta Eorzea es una web de Articulos y guias de Final Fantasy XIV en Español sin animo de lucro y con la idea de fomentar una comunidad hispana en el juego. Nos puedes seguir en nuestra cuenta de twitter para estar al dia de las actualizaciones de nuestros articulos. "
+    "body": "La Gaceta Eorzea es una web de Articulos y guias de Final Fantasy XIV en Español sin animo de lucro y con la idea de fomentar una comunidad hispana en el juego. Desde la creacion de la web nuestra idea ha sido estar libre de anuncios y abierta a todo el mundo para colaborar. Esperamos en breve publicar como podeis contribuir en cualquier parte de la web desde el contenido a el diseño de propia pagina. Nos podeis seguir en nuestra cuenta de twitter para estar al dia de las actualizaciones de nuestros articulos. "
     }, {
     "id": 2,
     "url": "/categories",
