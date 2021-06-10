c812956eb9a0854e774b92e610c8a1102ca73f6f
JohnyRisu <risugamer@gmail.com>
2021-06-10 19:00:58 +0200

    edicion discord

--- _pages/discord.md
+++ _pages/discord.md
@@ -4,7 +4,7 @@ title: Nuestro servidor de Discord.
 permalink: /discord
 comments: true
 ---
-Desde Gaceta Eorzea ponemos a disposición de la comunidad de Final Fantasy XIV nuestro discord. Queremos que sea un punto de encuentro los simpatizantes de Gazeta Eorzea.
+Desde Gaceta Eorzea ponemos a disposición de la comunidad de Final Fantasy XIV nuestro discord. Queremos que sea un punto de encuentro para los simpatizantes de Gazeta Eorzea.
 
 <p align="center">
 <img src="assets/images/logo.png" height="100"><a target="_blank" href="https://discord.gg/aKWGbAV7RV" class="btn btn-info">Unirte a nuestro discord <i class="fab fa-discord"></i></a>
--- docs/assets/js/lunrsearchengine.js
+++ docs/assets/js/lunrsearchengine.js
@@ -18,7 +18,7 @@ var documents = [{
     "id": 3,
     "url": "https://gaceta-eorzea.com/discord",
     "title": "Nuestro servidor de Discord.",
-    "body": "Desde Gaceta Eorzea ponemos a disposición de la comunidad de Final Fantasy XIV nuestro discord. Queremos que sea un punto de encuentro los simpatizantes de Gazeta Eorzea. Unirte a nuestro discord Si estás perdido dentro del juego o tienes dudas de cualquier mecánica, en nuestro discord podrás encontrar gente que te ayude, ya seas un nuevo jugador o un jugador veterano. Además, si tienes amplios conocimientos de Final Fantasy y quieres ayudar a otros jugadores, en nuestro discord encontrarás el entorno adecuado para hacerlo. "
+    "body": "Desde Gaceta Eorzea ponemos a disposición de la comunidad de Final Fantasy XIV nuestro discord. Queremos que sea un punto de encuentro para los simpatizantes de Gazeta Eorzea. Unirte a nuestro discord Si estás perdido dentro del juego o tienes dudas de cualquier mecánica, en nuestro discord podrás encontrar gente que te ayude, ya seas un nuevo jugador o un jugador veterano. Además, si tienes amplios conocimientos de Final Fantasy y quieres ayudar a otros jugadores, en nuestro discord encontrarás el entorno adecuado para hacerlo. "
     }, {
     "id": 4,
     "url": "https://gaceta-eorzea.com/",
