c8b78b3b27c4ad000acdedd006071d7da10a5f5c
Juan Medina <jamedina@gmail.com>
2023-03-05 19:36:27 +0000

    update for linux

---
+++ build.sh
@@ -0,0 +1,2 @@
+export JEKYLL_ENV=production
+bundle exec jekyll build --safe -d docs --config _config.yml,_pro.yml
---
+++ local.sh
@@ -0,0 +1,2 @@
+export JEKYLL_ENV=development
+bundle exec jekyll serve -d tmp --config _config.yml,_dev.yml
